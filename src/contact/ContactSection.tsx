729b1400fd6ebd953a81f3acd0291b768efbecd6
ramiferjani <ramiferjani.20.@gmail.com>
2025-05-06 21:04:23 +0100

    Fix fetch in cantact

--- src/contact/ContactSection.tsx
+++ src/contact/ContactSection.tsx
@@ -25,7 +25,7 @@ export default function ContactSection() {
     setIsSubmitting(true)
 
     try {
-      const response = await fetch('https://backendportfolio-5m1b.onrender.com', {
+      const response = await fetch('https://backendportfolio-5m1b.onrender.com/send-email', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(formData),
